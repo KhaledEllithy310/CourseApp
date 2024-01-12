37a472226d0ae8635575d4b6726f765d702bedf6
KhaledEllithy <khaledellessy310@gmail.com>
2024-01-13 00:45:21 +0200

    finish profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { NotifyService } from './../../services/notify.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,16 +8,49 @@ import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  constructor(private notifyService: NotifyService) {}
+  isValid: boolean = true;
+  interests: string[] = ['sports', 'swimming', 'fitness'];
   model = {
+    displayName: '',
     firstName: '',
     lastName: '',
-    email: '',
-    gender: '',
-    password: '',
+    about: '',
+    interests: {} as { [key: string]: boolean },
+    status: '',
+    experience: '',
+    expertise: {
+      backend: false,
+      frontend: false,
+    },
+    role: '',
   };
-  constructor(private formBuilder: FormBuilder) {}
 
-  handleSubmit(registerForm: NgForm) {
-    console.log(registerForm);
+  ngOnInit() {
+    let profileData = localStorage.getItem('profile');
+    if (profileData) this.model = JSON.parse(profileData);
+  }
+
+  handleSubmit(profileForm: NgForm) {
+    const { backend, frontend } = this.model.expertise;
+    const { status } = this.model;
+    if (status) {
+      if (
+        ((backend || frontend) && status === 'Professional') ||
+        (status === 'student' && !(backend && frontend))
+      ) {
+        this.isValid = true;
+        localStorage.setItem('profile', JSON.stringify(this.model));
+        this.notifyService.success('Your profile is updated.');
+        profileForm.reset();
+      } else {
+        this.isValid = false;
+        if (status === 'professional' && !(backend || frontend)) {
+          this.notifyService.error('Please select either Backend or Frontend.');
+        }
+      }
+    } else {
+      this.isValid = false;
+    }
   }
 }
